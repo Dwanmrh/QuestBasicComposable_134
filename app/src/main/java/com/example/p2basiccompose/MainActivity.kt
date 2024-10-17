207fc8500fac72149cdef748b60d3c9807d82b13
RidwanC134 <ridwanhh10@gmail.com>
2024-10-17 17:08:54 +0700

    Mengupdate beberapa import

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,8 +13,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.p2basiccompose.ui.theme.P2BasicComposeTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +46,12 @@ Column (
     horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Top,
     modifier = Modifier.fillMaxSize().padding(16.dp)
-)
+) {
+    Text(
+        text = "Login",
+        style = TextStyle(fontSize = 40.sp, fontWeight = FontWeight.Bold),
+    )
+}
 }
 
 @Preview(showBackground = true)
